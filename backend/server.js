4fa0bf0ba16deee971f4beadf718475d32b8a67e
mouhib <mouhibbenzina47@gmail.com>
2026-03-05 14:24:02 +0100

    Dynamic schedule by class and semester

--- backend/server.js
+++ backend/server.js
@@ -21,5 +21,5 @@ mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('✅ MongoDB connected'))
   .catch(err => console.error('❌ MongoDB error:', err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`🚀 Backend running on http://localhost:${PORT}`));
